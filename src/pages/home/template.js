58f780327e26c605021d93a66bb866634fe1af8b
Paul Noble <npaul98@vt.edu>
2018-08-06 13:50:27 -0400

    Some changes

--- src/pages/home/template.js
+++ src/pages/home/template.js
@@ -44,8 +44,8 @@ class HomeTemplate extends Component {
                 description: "A DESCRIPTION OF DUTIES WOULD GO HERE"
             },
             {
-                title: "faggot",
-                organization: "gay inc.",
+                title: "person",
+                organization: "the guy inc.",
                 dates: {
                     start: "July 2017"
                 },
